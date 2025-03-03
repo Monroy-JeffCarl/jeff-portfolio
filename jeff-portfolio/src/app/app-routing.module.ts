b63618bfea3a6bfc59e867e7d30d5be774ee0109
Jeff Carl Monroy <jeffcmonroy@gmail.com>
2025-03-04 02:24:42 +0800

    modify entry module

--- jeff-portfolio/src/app/app-routing.module.ts
+++ jeff-portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EntryComponent } from './entry/entry.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'entry', component: EntryComponent },
   { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/entry', pathMatch: 'full' }
 ];
 
 @NgModule({
--- jeff-portfolio/src/app/entry/entry.component.ts
+++ jeff-portfolio/src/app/entry/entry.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
-  styleUrl: './entry.component.css'
+  styleUrls: ['./entry.component.css']
 })
 export class EntryComponent {
+  constructor(private router: Router) {}
 
+  openPortfolio() {
+    this.router.navigate(['/home']);
+  }
 }
